1c0471d84c09baef9d5ed075d0d2f5d20a592d07
JovitoAddo <jovitoaddo@gmail.com>
2022-03-29 09:31:20 +0700

    commit early crud

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const usersRouter = require("./users.route");
-const booksRouter = require("./books.route");
-const transactionsRouter = require("./transactions.route");
+// const usersRouter = require("./users.route");
+const booksRouter = require("./books.router");
+// const transactionsRouter = require("./transactions.route");
 
 router.get("/", (req, res) => {
   res.json("ini dari server");
 });
 
-router.use("/users", usersRouter);
+// router.use("/users", usersRouter);
 router.use("/books", booksRouter);
-router.use("/transactions", transactionsRouter);
+// router.use("/transactions", transactionsRouter);
 
 module.exports = router;
